85683cb0ad8a5ef83a9f73cf0a6fe3d93a9f7f98
HossainRafi <hossainrafi71@gmail.com>
2022-11-14 21:46:38 +0600

    hero section structure

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <Hero />
-      <HeadlineCards />
-      <Food />
-      <Category /> */}
+      <Hero />
+      {/* <HeadlineCards /> */}
+      {/* <Food /> */}
+      {/* <Category /> */}
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="max-w-[1640px] mx-auto p-4">
+      <div className="max-h-[500px] relative">
+        
+        <img
+          className="w-full max-h-[500px] object-cover"
+          src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
