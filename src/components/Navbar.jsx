e7c466800d2c7833fed67ac2a4ce34c85df82b0f
HossainRafi <hossainrafi71@gmail.com>
2022-11-14 20:51:40 +0600

    making menubar functional

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AiOutlineMenu,
   AiOutlineSearch,
@@ -11,20 +11,18 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
-      {/* Left Side */}
+      {/* Menu */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 font-serif cursor-pointer">
           Food <span className="font-bold">King</span>
         </h1>
-        {/* <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
-          <p className="bg-black text-white rounded-full p-2">Delivery</p>
-          <p className="p-2">Pickup</p>
-        </div> */}
       </div>
 
       {/* Search Input */}
@@ -43,15 +41,22 @@ const Navbar = () => {
       </button>
 
       {/* Mobile Menu */}
-      {/* Overlay */}
+      {nav ? (
         <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
 
-      {/* Side drawer menu */}
+      {/* Side Menu */}
       <div
-        className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
-        
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
       >
         <AiOutlineClose
+          onClick={() => setNav(!nav)}
           size={30}
           className="absolute right-4 top-4 cursor-pointer"
         />
