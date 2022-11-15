61821e4f78c4232c4f7963136248da2906d7bf75
HossainRafi <hossainrafi71@gmail.com>
2022-11-15 12:03:09 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
 import Category from "./components/Category";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <HeadlineCards />
       <Food />
       <Category />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-200 py-5'>
+      <p className='text-center'>All Right Reserved By &#169;<span>Rafi</span></p>
+      
+    </div>
+  );
+}
+
+export default Footer
