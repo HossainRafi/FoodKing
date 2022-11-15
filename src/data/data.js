1660247d1506043f966aa4d5a079f80657d3316a
HossainRafi <hossainrafi71@gmail.com>
2022-11-15 09:17:31 +0600

    data created

---
+++ src/data/data.js
@@ -0,0 +1,165 @@
+export const data = [
+  {
+    id: 1,
+    name: "Double Cheeseburger",
+    category: "burger",
+    image: ".././images/burger1.jpg",
+    price: "$$$$",
+  },
+  {
+    id: 2,
+    name: "Bacon Cheeseburger",
+    category: "burger",
+    image: ".././images/burger2.jpg",
+    price: "$",
+  },
+  {
+    id: 3,
+    name: "Mushroom Burger",
+    category: "burger",
+    image: ".././images/burger3.jpg",
+    price: "$$",
+  },
+  {
+    id: 4,
+    name: "Loaded Burger",
+    category: "burger",
+    image: ".././images/burger4.jpg",
+    price: "$$$",
+  },
+  {
+    id: 5,
+    name: "Feta & Spinnach",
+    category: "pizza",
+    image: ".././images/pizza1.jpg",
+    price: "$$",
+  },
+  {
+    id: 6,
+    name: "Supreme Pizza",
+    category: "pizza",
+    image: ".././images/pizza2.jpg",
+    price: "$$$",
+  },
+  {
+    id: 7,
+    name: "Meat Lovers",
+    category: "pizza",
+    image: ".././images/pizza3.jpg",
+    price: "$$$$",
+  },
+  {
+    id: 8,
+    name: "Cheese Pizza",
+    category: "pizza",
+    image: ".././images/pizza4.jpg",
+    price: "$",
+  },
+  {
+    id: 9,
+    name: "Kale Salad",
+    category: "salad",
+    image: ".././images/salad1.jpg",
+    price: "$$",
+  },
+  {
+    id: 10,
+    name: "Ceasar Salad",
+    category: "salad",
+    image: ".././images/salad2.jpg",
+    price: "$$$",
+  },
+  {
+    id: 11,
+    name: "Loaded Salad",
+    category: "salad",
+    image: ".././images/salad3.jpg",
+    price: "$$$$",
+  },
+  {
+    id: 12,
+    name: "Fruit Salad",
+    category: "salad",
+    image: ".././images/salad4.jpg",
+    price: "$",
+  },
+  {
+    id: 13,
+    name: "Wings",
+    category: "chicken",
+    image: ".././images/chicken1.jpg",
+    price: "$$",
+  },
+  {
+    id: 14,
+    name: "Baked Chicken",
+    category: "chicken",
+    image: ".././images/chicken2.jpg",
+    price: "$$$$",
+  },
+  {
+    id: 15,
+    name: "Chicken Tenders",
+    category: "chicken",
+    image: ".././images/chicken3.jpg",
+    price: "$",
+  },
+  {
+    id: 16,
+    name: "Chicken Kabob",
+    category: "chicken",
+    image: ".././images/chicken4.jpg",
+    price: "$$$",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Fast Food",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png",
+  },
+  {
+    id: 3,
+    name: "Wings",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png",
+  },
+  {
+    id: 4,
+    name: "Indian",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png",
+  },
+  {
+    id: 5,
+    name: "Latest Deals",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png",
+  },
+  {
+    id: 6,
+    name: "Restaurant Rewards",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png",
+  },
+  {
+    id: 7,
+    name: "Best Overall",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png",
+  },
+  {
+    id: 8,
+    name: "Shipped Free",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png",
+  },
+];
