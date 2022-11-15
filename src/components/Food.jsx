a42e47f90973695a3644228423d9528e1b036ea6
HossainRafi <hossainrafi71@gmail.com>
2022-11-15 09:56:18 +0600

    filter button added

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -9,7 +9,48 @@ const Food = () => {
       <h1 className="text-orange-600 font-bold text-4xl text-center">
         Top Rated Food Items
       </h1>
-
+      <div className="flex flex-col lg:flex-row justify-between">
+        <div>
+          <p className="font-bold text-gray-700">Filter Type</p>
+          <div className="flex justfiy-between flex-wrap">
+            <button
+              onClick={() => setFoods(data)}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              All
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              Burgers
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              Pizza
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              Salads
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              Chicken
+            </button>
+          </div>
+        </div>
+        <div>
+          <p className="font-bold text-gray-700">Filter Price</p>
+          <div className="flex justify-between max-w-[390px] w-full">
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              $
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              $$
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              $$$
+            </button>
+            <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+              $$$$
+            </button>
+          </div>
+        </div>
+      </div>
       {/* Display foods */}
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
         {foods.map((item, index) => (
